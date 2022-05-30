685eabfb15113214f4c57992cde4650ac9a2b365
Rafaela Nascimento <contato.karinenasc@gmail.com>
2022-05-30 19:25:22 -0300

    Adicionando novos dados nos gráficos, alterando cores e simetria

--- startbootstrap-sb-admin-master/dist/assets/demo/chart-area-demo.js
+++ startbootstrap-sb-admin-master/dist/assets/demo/chart-area-demo.js
@@ -11,10 +11,10 @@ var myLineChart = new Chart(ctx, {
     datasets: [{
       label: "Casos: ",
       lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
+      backgroundColor: "rgba(206,133,123,0.8)",
+      borderColor: "rgba( 26,37,48,1)",
       pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBackgroundColor: "rgba(243,51,58,1)",
       pointBorderColor: "rgba(255,255,255,0.8)",
       pointHoverRadius: 5,
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
